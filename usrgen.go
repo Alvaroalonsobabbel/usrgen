7cc1e5fd9c908a9989926ab6be9df5e14dda339a
Alvaro <47629098+Alvaroalonsobabbel@users.noreply.github.com>
2024-07-11 08:24:49 +0200

    refactored Generate()

--- usrgen.go
+++ usrgen.go
@@ -30,7 +30,7 @@ func New(firstName, lastName, lang string) *ug {
 
 // Generate will return a username and an error.
 // For each time the function is called, it will return a new
-// username with an additional caracter from the first name.
+// username with an additional letter from the first name.
 //
 // An error will be returned if the function is called more
 // times than the length of the first name.
@@ -38,9 +38,11 @@ func (u *ug) Generate() (string, error) {
 	if u.n > len(u.first) {
 		return "", fmt.Errorf(errMsg)
 	}
+	defer u.increment()
 
-	name := u.first[:u.n] + u.last
+	return u.first[:u.n] + u.last, nil
+}
+
+func (u *ug) increment() {
 	u.n++
-
-	return name, nil
 }
